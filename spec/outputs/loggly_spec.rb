94f49c039fd7de05057cc6ace5d97dd61e2aa089
nywilken <wilken@cnevids.com>
2015-03-03 17:35:48 -0500

    Update tests to use rspec 3 syntax.

--- spec/outputs/loggly_spec.rb
+++ spec/outputs/loggly_spec.rb
@@ -3,57 +3,52 @@ require "logstash/devutils/rspec/spec_helper"
 require "logstash/outputs/loggly"
 
 describe 'outputs/loggly' do
-  let(:config) { { "key" => '34567d8-2b67-567-fghj-6789' } }
+  let(:config) { { 'key' => 'abcdef123456' } }
   let(:event) { LogStash::Event.new({'message' => 'fanastic log entry', 'source' => 'someapp', 'type' => 'nginx',
                                      '@timestamp' => LogStash::Timestamp.now}) }
 
   context 'when initializing' do
+    subject { LogStash::Outputs::Loggly.new(config) }
+
     it 'should register' do
-      output = LogStash::Outputs::Loggly.new(config)
-      expect {output.register}.to_not raise_error
+      expect { subject.register }.to_not raise_error
     end
 
-    it 'should populate config with defalt values' do
-      output = LogStash::Outputs::Loggly.new(config)
-      insist { output.proto } == 'http'
-      insist { output.host } == 'logs-01.loggly.com'
-      insist { output.tag } == 'logstash'
+    it 'should have default config values' do
+      insist { subject.proto } == 'http'
+      insist { subject.host } == 'logs-01.loggly.com'
+      insist { subject.tag } == 'logstash'
     end
   end
 
   context "when outputting messages" do
-    it 'should support field interpolation for key' do
-      event['token'] = '5678-ty67-hbj6789-9876h'
-      config['key'] = "%{token}"
+    it 'should support field interpolation on key' do
+      # add a custom key value for Loggly config
+      event['token'] = 'xxxxxxx1234567'
+      config['key'] = '%{token}'
+
       output = LogStash::Outputs::Loggly.new(config)
-      expected_message = event.to_json
-      output.should_receive(:send_event).with("http://logs-01.loggly.com/inputs/5678-ty67-hbj6789-9876h/tag/logstash", expected_message)
+      allow(output).to receive(:send_event).with("http://logs-01.loggly.com/inputs/xxxxxxx1234567/tag/logstash", event.to_json)
       output.receive(event)
     end
 
     it 'should set the default tag to logstash' do
       output = LogStash::Outputs::Loggly.new(config)
-      expected_message = event.to_json
-      insist { output.format_message(event) } == expected_message
-      output.should_receive(:send_event).with("http://logs-01.loggly.com/inputs/34567d8-2b67-567-fghj-6789/tag/logstash", expected_message)
+      allow(output).to receive(:send_event).with("http://logs-01.loggly.com/inputs/abcdef123456/tag/logstash", event.to_json)
       output.receive(event)
     end
   
     it 'should support field interpolation for tag' do
-      config['tag'] = "%{source}"
+      config['tag'] = '%{source}'
       output = LogStash::Outputs::Loggly.new(config)
-      expected_message = event.to_json
-      insist { output.format_message(event) } == expected_message
-      output.should_receive(:send_event).with("http://logs-01.loggly.com/inputs/34567d8-2b67-567-fghj-6789/tag/someapp", expected_message)
+      allow(output).to receive(:send_event).with("http://logs-01.loggly.com/inputs/abcdef123456/tag/someapp", event.to_json)
       output.receive(event)
     end
 
     it 'should default tag to logstash if interpolated field does not exist' do
-      config['tag'] = "%{foobar}"
+      config['tag'] = '%{foobar}'
       output = LogStash::Outputs::Loggly.new(config)
-      expected_message = event.to_json
-      insist { output.format_message(event) } == expected_message
-      output.should_receive(:send_event).with("http://logs-01.loggly.com/inputs/34567d8-2b67-567-fghj-6789/tag/logstash", expected_message)
+      allow(output).to receive(:send_event).with("http://logs-01.loggly.com/inputs/abcdef123456/tag/logstash", event.to_json)
       output.receive(event)
     end
   end
